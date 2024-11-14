6e12fbfd2d9d1f71ebea367a87c788e90cddeac1
Evgeniya1311 <zhe50404156@gmail.com>
2024-11-14 11:21:21 +0300

    Add files via upload
    Практическое задание по теме "Переменные"

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+course_title = 'Python'
+time_for_one_task = homework_count / hours_count
+print('Курс: ', course_title, ', всего задач: ', homework_count, ', затрачено часов: ', hours_count, ', среднее время выполнения: ', time_for_one_task, ' часов')
