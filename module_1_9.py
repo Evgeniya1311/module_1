fdd6331d7265962ff6d951e0d1d13ce2da292f43
Evgeniya1311 <zhe50404156@gmail.com>
2024-11-15 11:46:55 +0300

    Add files via upload
    Практическое задание по уроку "Строки и индексация строк"

---
+++ module_1_9.py
@@ -0,0 +1,6 @@
+example = 'Simba'
+print(example[0]) #первый символ
+print(example[-1])  #последний символ
+print(example[2:])  #вторая половина строки
+print(example[::-1])    #слово наоборот
+print(example[1::2])    #каждый воторой символ
