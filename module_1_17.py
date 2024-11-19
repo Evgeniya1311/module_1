c5b6f19a5180048834f778c11d5ab56937c99702
Evgeniya1311 <zhe50404156@gmail.com>
2024-11-19 13:21:35 +0300

    Add files via upload
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ module_1_17.py
@@ -0,0 +1,9 @@
+immutable_var = ('hedgehog', 27, 4, True, 8.5)
+print(immutable_var)
+
+# immutable_var[1] = 5  - эл-ты кортежа неизменямы
+
+mutable_list = ['Mr. Credo', 'peach', 8, '3']
+mutable_list[-1] = 5
+print(mutable_list)
+
